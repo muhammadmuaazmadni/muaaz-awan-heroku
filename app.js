06bd0c1bff9d92011a3d0a30b92df855fe6cde95
mdjunaid <sohailjunaid220@gmail.com>
2020-12-18 16:18:13 +0500

    some code

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var server = express();
+
+server.get("/", (req, res, next) => {
+    console.log("some one get menu");
+    res.send("menu: what do you want? food or water");
+})
+server.get("/water", (req, res, next) => {
+    console.log("some one is asking water");
+    res.send("here is water");
+})
+
+server.get("/food", (req, res, next) => {
+    console.log("some one is asking food");
+    res.send("here is food");
+})
+server.get("/juice",(req,res,next) =>{
+    console.log("some one is asking juice ")
+    res.send("juice is also available")
+})
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
